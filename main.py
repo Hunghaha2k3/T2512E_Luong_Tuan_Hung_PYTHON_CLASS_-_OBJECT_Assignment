58e04f14c2543a445ced307320795f64115d3374
Hunghaha1608 <diepiepew2018@gmail.com>
2026-09-17 21:18:38 +0700

    Bài tập Class & Object

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from python_class.Nhan_vien import NhanVien
+
+
+if __name__ == "__main__":
+    nv1 = NhanVien("Luong Hung", 10)
+    print(f"{nv1.__str__()}")
+
+    nv2 = NhanVien("Ngo Nam", 15)
+    print(f"{nv2.__str__()}")
+
